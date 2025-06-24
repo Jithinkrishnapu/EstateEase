492d84ebec0fac96961fb1b936ba7cc271f9d6f5
jithin krishna <jithinkrishna@jithins-MacBook-Air.local>
2025-06-24 21:31:24 +0530

    Update app configuration and dependencies; refactor App component to use RouteNavigator. Added nativewind-env.d.ts for NativeWind support. Updated tailwind.config.js to include new file paths for NativeWind classes.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import RouteNavigator from "~/routes/RouteNavigator";
+import "./global.css"
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+   <RouteNavigator/>
   );
 }
